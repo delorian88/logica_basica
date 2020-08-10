8f7032e6a7a0d5caa353a5b1c37d1f5b4ffb9919
Del <del.kaizen@gmail.com>
2020-08-09 20:50:16 -0400

    Finalizando IMC com comentários

--- imc.dart
+++ imc.dart
@@ -1,21 +1,33 @@
 import 'dart:io';
 
 main() {
+  // Corpo do Algoritmo.
+
   print('Bem Vindo ao IMC');
+
+  // Recebe o valor de peso.
   print('=====Digite o seu Peso:');
+  String textPeso = stdin.readLineSync();
+  int peso = int.parse(textPeso);
 
-  var textPeso = stdin.readLineSync();
-  var peso = int.parse(textPeso);
-
+  // Recebe o valor da altura.
   print('=====Digite o sua altura:');
-  var textAltura = stdin.readLineSync();
-  var altura = double.parse(textAltura);
+  String textAltura = stdin.readLineSync();
+  double altura = double.parse(textAltura);
 
-  var calImc = peso / (altura * altura);
+  // Recebe os valores do peso e altura para função.
+  double imc = calImc(peso, altura);
 
-  resultado(calImc);
+  // Apresenta o resultado do IMC.
+  resultado(imc);
 }
 
+// Função de calculo do IMC
+double calImc(int peso, double altura) {
+  return peso / (altura * altura);
+}
+
+// função com retorno do Resultado do IMC
 resultado(double calImc) {
   if (calImc <= 18.5) {
     print('Magreza');
